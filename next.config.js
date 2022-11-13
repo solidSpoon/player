b4dd24cbb80149f51a85038819f9b5ca63f9a869
solidspoon <cedarrival@outlook.com>
2022-11-13 22:58:03 +0800

    配置文件模版

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+    reactStrictMode: true,
 }
 
 module.exports = nextConfig
+
+
+module.exports = {
+    serverRuntimeConfig: {
+        // Will only be available on the server side
+        secretId: '',
+        secretKey: ''
+    },
+    publicRuntimeConfig: {
+        // Will be available on both server and client
+        staticFolder: '/static',
+    },
+}
