162f1175551146c8e8b0303726ce9b038a6016ca
solidspoon <cedarrival@outlook.com>
2022-11-13 17:28:21 +0800

    隔离密钥

--- pages/api/translate.js
+++ pages/api/translate.js
@@ -1,14 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
+import getConfig from 'next/config'
 export default function handler(req, res) {
-    console.log(req);
-// 导入对应产品模块的client models。
+    const { serverRuntimeConfig, publicRuntimeConfig } = getConfig()
     const tencentcloud = require("tencentcloud-sdk-nodejs")
     const TmtClient = tencentcloud.tmt.v20180321.Client;
     const clientConfig = {
         credential: {
-            secretId: "AKIDMaaKo34tVt3zg8LQCiz0nLejcIjvsZ0B",
-            secretKey: "1oMglbCYgbIkxvd5Hn58vSjkUyFJ8i27",
+            secretId: serverRuntimeConfig["secretId"],
+            secretKey: serverRuntimeConfig["secretKey"],
         },
         region: "ap-shanghai",
     }
--- pages/index.js
+++ pages/index.js
@@ -107,7 +107,7 @@ export default function Home() {
 
     const updateSubtitle = (str) => {
         const srtSubtitles = parseSrtSubtitles(str);
-        fillTranslate(srtSubtitles, setSubtitles);
+        fillTranslate(srtSubtitles);
         setSubtitles(srtSubtitles);
     };
 
