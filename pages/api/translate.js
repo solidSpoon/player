e09d707c473783f7b4d8588c9b943499bab1531e
cedarrival <cedarrival@foxmail.com>
2022-11-14 15:40:53 +0800

    缓存翻译结果

--- pages/api/translate.js
+++ pages/api/translate.js
@@ -1,35 +1,48 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import getConfig from 'next/config'
+import Trans from "../../lib/Trans";
+import Cache from "../../lib/Cache";
+
 export default function handler(req, res) {
-    const { serverRuntimeConfig, publicRuntimeConfig } = getConfig()
-    const tencentcloud = require("tencentcloud-sdk-nodejs")
-    const TmtClient = tencentcloud.tmt.v20180321.Client;
-    const clientConfig = {
-        credential: {
-            secretId: serverRuntimeConfig["secretId"],
-            secretKey: serverRuntimeConfig["secretKey"],
-        },
-        region: "ap-shanghai",
-    }
-    const client = new TmtClient(clientConfig)
-    client.TextTranslateBatch({
-            Source: 'en',
-            Target: 'zh',
-            ProjectId: 0,
-            SourceTextList: req.body.str
+    const crypto = require('crypto');
+    let sourceArr = req.body.str.map(item => {
+        item = item.trim();
+        return {
+            text: item,
+            hash: crypto.createHash('md5').update(item).digest("hex"),
+            translate: undefined,
         }
-    ).then(
-        (data) => {
+    });
+
+    const c = new Cache();
+    c.batchQuery(sourceArr.map((item) => item["hash"]), processQueryResult)
+
+    function processQueryResult(dbDocs) {
+        dbDocs.forEach((doc) => {
+            const find = sourceArr.find((item) => item["hash"] === doc["hash"]);
+            find["translate"] = doc["translate"];
+        })
+
+        const retryDoc = sourceArr.filter((doc) => doc.translate === undefined);
+        if (retryDoc.length === 0) {
             res.status(200).json({
                 success: true,
-                strs: data.TargetTextList
-            })
-        },
-        (err) => {
-            res.status(500).json({
-                success: false,
-                err: err
-            })
+                strs: sourceArr.map(doc => doc.translate)
+            });
+            return;
         }
-    )
+        const trans = new Trans();
+        trans.batchTrans(retryDoc, () => processTransResult(retryDoc));
+    }
+
+    function processTransResult(retryDoc) {
+        c.insert(retryDoc, processResult)
+    }
+
+    function processResult() {
+        res.status(200).json({
+            success: true,
+            strs: sourceArr.map(doc => doc.translate)
+        });
+    }
 }
+
