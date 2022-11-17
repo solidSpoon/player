a03c2223a02027a525a0cc5850ef913e913c78ce
cedarrival <cedarrival@foxmail.com>
2022-11-17 17:38:07 +0800

    调整播放器参数

--- components/Player.tsx
+++ components/Player.tsx
@@ -4,36 +4,34 @@ import style from './Player.module.css'
 
 
 const Player = ({playerRef, currentTimeState, videoFile, playingState, totalTmeState}) => {
-    const [player, setPlayer] = useState<ReactElement>()
     const [, setCurrentTime] = currentTimeState;
-    const [playing, setPlaying] = playingState
+    const [, setPlaying] = playingState
     const [, setTotalTime] = totalTmeState
-    let reactPlayer = <>
-        <ReactPlayer
-            id="react-player-id"
-            ref={playerRef}
-            url={videoFile ? videoFile : ""}
-            className={"react-player" + style.player}
-            playing={playing}
-            controls={false}
-            width="100%"
-            height="100%"
-            onProgress={(progress) => {
-                setCurrentTime(progress.playedSeconds.toFixed(3));
-            }}
-            onDuration={duration => {
-                setTotalTime(duration);
-            }}
-            onPlay={() => setPlaying(true)}
-            onPause={() => setPlaying(false)}
-        />
-    </>;
 
-    useEffect(() => setPlayer(reactPlayer), [])
     return (
-        <>
-            {player}
-        </>
+
+            <>
+                <ReactPlayer
+                    id="react-player-id"
+                    ref={playerRef}
+                    url={videoFile ? videoFile : ""}
+                    className={"react-player" + style.player}
+                    playing={true}
+                    controls={false}
+                    width="100%"
+                    height="100%"
+                    progressInterval={50}
+                    onProgress={(progress) => {
+                        setCurrentTime(progress.playedSeconds);
+                    }}
+                    onDuration={duration => {
+                        setTotalTime(duration);
+                    }}
+                    onPlay={() => setPlaying(true)}
+                    onPause={() => setPlaying(false)}
+                />
+            </>
+
     )
 
 }
