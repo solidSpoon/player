157cbf7fddf7a9d30b1a5280a413c1a7dbea50e8
cedarrival <cedarrival@foxmail.com>
2022-11-21 15:13:05 +0800

    Player切换为Class组件

--- components/Player.tsx
+++ components/Player.tsx
@@ -1,51 +1,72 @@
 import ReactPlayer from 'react-player'
-import React from 'react'
+import React, {Component} from 'react'
 import style from './Player.module.css'
-import PlayerParam, {playState} from "../lib/param/PlayerParam";
 import axios from "axios";
+import FileT from "../lib/param/FileT";
+interface PlayerParam {
+    videoFile: FileT;
+    onProgress: (time: number) => void;
+    onPlayingStateChange: (state: playState) => void;
+    onTotalTimeChange:(time:number)=>void;
+}
+export enum playState {
+    play = 1,
+    pause = 0
+}
+interface PlayerState{
 
-const Player = (param: PlayerParam) => {
-    let isReady = false;
-    const onReady = () => {
-        if (isReady) {
+}
+export default class Player extends Component<PlayerParam, PlayerState>{
+    private readonly playerRef: React.MutableRefObject<ReactPlayer | undefined>;
+    constructor(props) {
+        super(props);
+        this.playerRef =React.createRef<ReactPlayer>();
+    }
+    public seekTo(time:number) {
+        this.playerRef.current.seekTo(time,"seconds");
+    }
+    private isReady = false;
+    private onReady = () => {
+        if (this.isReady) {
             return;
         }
         console.log('isready')
         axios.get('/api/queryProgress', {
             params:
                 {
-                    fileName: param.videoFile.fileName
+                    fileName: this.props.videoFile.fileName
                 }
         }).then((response) => {
             console.log(response);
-            param.playerRef.current.seekTo(response.data.progress, "seconds");
+            this.seekTo(response.data.progress);
         });
-        isReady = true;
+        this.isReady = true;
     };
 
-    return (
-        <ReactPlayer
-            id="react-player-id"
-            ref={param.playerRef}
-            url={param.videoFile.objectUrl ? param.videoFile.objectUrl : ""}
-            className={"react-player" + style.player}
-            playing={true}
-            controls={false}
-            width="100%"
-            height="100%"
-            progressInterval={50}
-            onProgress={(progress) => {
-                param.onProgress(progress.playedSeconds);
-            }}
-            onDuration={duration => {
-               param.onTotalTimeChange(duration)
-            }}
-            onPlay={() => param.onPlayingStateChange(playState.play)}
-            onPause={() => param.onPlayingStateChange(playState.pause)}
-            onReady={onReady}
-        />
+    render() {
+        return (
+            <ReactPlayer
+                id="react-player-id"
+                ref={this.playerRef}
+                url={this.props.videoFile.objectUrl ? this.props.videoFile.objectUrl : ""}
+                className={"react-player" + style.player}
+                playing={true}
+                controls={false}
+                width="100%"
+                height="100%"
+                progressInterval={50}
+                onProgress={(progress) => {
+                    this.props.onProgress(progress.playedSeconds);
+                }}
+                onDuration={duration => {
+                    this.props.onTotalTimeChange(duration)
+                }}
+                onPlay={() => this.props.onPlayingStateChange(playState.play)}
+                onPause={() => this.props.onPlayingStateChange(playState.pause)}
+                onReady={this.onReady}
+            />
 
-    )
+        )
 
+    }
 }
-export default Player
