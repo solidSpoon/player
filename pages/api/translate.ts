f16c82d5e557999e2fddfcae851e7bd2e3e81282
cedarrival <cedarrival@foxmail.com>
2022-11-25 17:58:08 +0800

    翻译api代码优化

--- pages/api/translate.ts
+++ pages/api/translate.ts
@@ -10,34 +10,33 @@ interface TranslateParam {
     }
 }
 
-export default function handler(req: TranslateParam, res) {
+export default async function handler(req: TranslateParam, res) {
     const c = new Cache();
     const trans = new Trans();
 
     let sourceArr: cacheEntity[] = req.body.str.map(item => new CacheEntity(item));
     sourceArr.forEach(item => item.hash = c.hash(item.text));
-    const process = async (sourceArr: cacheEntity[]): Promise<CacheEntity[]> => {
-        const dbDocs: CacheEntity[] = await c.getCache(sourceArr.map(item => item.text))
-            .then(dbDoc => dbDoc.filter(item => item.translate !== undefined));
 
-        dbDocs.forEach((doc) => {
-            const finds: CacheEntity[] = sourceArr.filter((item) => item.hash === doc.hash);
-            finds.forEach(find => find.translate = doc.translate);
-        })
-        const retryDoc: CacheEntity[] = sourceArr.filter((doc) => doc.translate === undefined);
-        if (retryDoc.length === 0) {
-            return sourceArr;
-        }
-        const transResult: CacheEntity[] = await trans.batchTrans2(retryDoc);
-        await c.insert(transResult);
-        return sourceArr;
-    }
-    process(sourceArr)
-        .then(docs => {
-            res.status(200).json({
-                success: true,
-                strs: docs.map(doc => doc.translate)
-            });
+    const dbDocs: CacheEntity[] = await c.getCache(sourceArr.map(item => item.text))
+        .then(dbDoc => dbDoc.filter(item => item.translate !== undefined));
+
+    dbDocs.forEach((doc) => {
+        const finds: CacheEntity[] = sourceArr.filter((item) => item.hash === doc.hash);
+        finds.forEach(find => find.translate = doc.translate);
+    })
+    const retryDoc: CacheEntity[] = sourceArr.filter((doc) => doc.translate === undefined);
+    if (retryDoc.length === 0) {
+        return res.status(200).json({
+            success: true,
+            strs: sourceArr.map(doc => doc.translate)
         });
+    }
+    const transResult: CacheEntity[] = await trans.batchTrans2(retryDoc);
+    await c.insert(transResult);
+    return res.status(200).json({
+        success: true,
+        strs: sourceArr.map(doc => doc.translate)
+    });
+
 }
 
