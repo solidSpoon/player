2a32182744ab811756f9c4f84f07b60925648796
cedarrival <cedarrival@foxmail.com>
2022-11-25 20:50:33 +0800

    backup

--- ServerLib/ProgressCache.ts
+++ ServerLib/ProgressCache.ts
@@ -1,13 +1,13 @@
 import CacheEntity from "./CacheEntity";
 import ProgressEntity from "./ProgressEntity";
-import NeDB from "nedb";
 import {string} from "prop-types";
 import crypto from "crypto";
+import Nedb from "nedb";
 
 class ProgressCache {
-    private static db: NeDB<ProgressEntity>;
+    private static db: Nedb<ProgressEntity>;
     static {
-        this.db = new NeDB(
+        this.db = new Nedb(
             {
                 filename: './db/progress.db',
                 autoload: true,
@@ -15,7 +15,7 @@ class ProgressCache {
         )
     }
 
-    public static updateProgress(progress: ProgressEntity): void {
+    public static async updateProgress(progress: ProgressEntity): void {
         progress.updateDate = Date.now();
         const tdb = this.db;
         console.log(progress)
@@ -34,6 +34,41 @@ class ProgressCache {
                 })
             }
         })
+
+       const updateNum = await this.updateOne(progress);
+        if (updateNum > 1) {
+            console.log(">1")
+            tdb.remove({
+                hash: progress.hash
+            }, {multi: true}, function (error, docs) {
+                console.log(error)
+                console.log(docs)
+                tdb.insert(progress);
+            })
+        }
+    }
+
+    private static async deleteProgress(fileName: string) {
+
+    }
+
+    private static async updateOne(progress: ProgressEntity): Promise<number> {
+        progress.updateDate = Date.now();
+        console.log("update progress", progress);
+        const query = {
+            hash: progress.hash
+        }
+        const options: Nedb.UpdateOptions = {
+            upsert: true,
+            multi: true
+        }
+
+        return new Promise((resolve, reject) => {
+            this.db.update(query, progress, options, (error, num) => {
+                console.log("update item", num);
+                resolve(num);
+            })
+        });
     }
 
     public static async queryProcess(progress: ProgressEntity): Promise<void> {
